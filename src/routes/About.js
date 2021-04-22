93b486a749c7ad6d127b6020517163d5f229c3df
sean <seankim21@naver.com>
2021-04-22 20:22:20 +0900

    The End :)

---
+++ src/routes/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <span>
+      <strong>About this page:</strong> I built it because I love movies.
+    </span>
+  );
+}
+
+export default About;
